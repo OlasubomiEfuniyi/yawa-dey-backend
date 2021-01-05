d9cb88ad69ac834934a04eb6f28717b0b415eae3
Olasubomi Efuniyi <subsefun@gmail.com>
2021-01-05 17:43:32 -0500

    Added seasons and series endpoint. Fine tuned episodes endpoint to handle filter and search

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongodb = require("mongodb");
+const e = require("express");
 
 const app = express();
 const port = 8080;
@@ -19,15 +20,36 @@ app.use(express.json());
 */
 app.post("/episodes", (req, res) => {
     console.log("request made for episodes");
-    handleEpisodeRequest(req, res);
+    handleEpisodesRequest((episodes, client) => processEpisodes(req.body, episodes, res, client));
+});
+
+
+/*
+    This enpoint handles a request for a list of all the season numbers.
+*/
+app.get("/seasons", (req, res) => {
+    console.log("request made for seasons");
+    handleEpisodesRequest((episodes, client) => processSeasons(episodes, res, client));
+});
+
+/*
+    This endpoint handles a request for a list of all the series.
+*/
+app.get("/series", (req, res) => {
+    console.log("request made for series");
+    handleEpisodesRequest((episodes, client) => processSeries(episodes, res, client));
 });
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`)
 });
 
-
-async function handleEpisodeRequest(req, res) {
+/*
+    This function handles the creation of a collection object that represents the episodes collection. If successful, it invokes the 
+    callback, passing it the episodes collection object and the client used to access it. When the callback is complete, the client must
+    be closed using client.close().
+*/
+async function handleEpisodesRequest(callback) {
     const MongoClient = mongodb.MongoClient;
     const uri = "mongodb+srv://olasubomi:j5ThIonON4Z2g1MW@cluster0.ouakf.mongodb.net/yawa-dey?retryWrites=true&w=majority";
     const client = await new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
@@ -42,7 +64,7 @@ async function handleEpisodeRequest(req, res) {
                     console.log("could not obtain episodes collection");
                 } else {
                     console.log("obtained episodes collection");
-                    processEpisodes(req.body, episodes, res, client); 
+                    callback(episodes, client); 
                 }
             });
         }
@@ -53,18 +75,22 @@ async function processEpisodes(body, episodes, res, client) {
     console.log(`Querying the episodes collection for documents matching ${JSON.stringify(body)}`);
     let cursor = null;
 
-    if(body.seasonNumber == null) {
-        if(body.series == null) { //no season number and series was provided. Return all episodes
-            cursor = await episodes.find({});
-        } else { //series was provided. Return all episodes with the provided series tag
-            cursor = await episodes.find({series: body.series});
+    if(body.filtered) { //You can look for filter keys in the body of the request
+        if(body.seasonNumber == null) {
+            if(body.series == null) { //no season number and series was provided. Return all episodes
+                cursor = await episodes.find({});
+            } else { //series was provided. Return all episodes with the provided series tag
+                cursor = await episodes.find({series: body.series});
+            }
+        } else { //season number was provided. Return all episodes with the provided season number
+            if(body.series == null) { //no series was provided. Return all episodes with provided season number
+                cursor = await episodes.find({seasonNumber: body.seasonNumber});
+            } else { //series was provided. Return all episodes with the provided seasonNumber and series tag
+                cursor = await episodes.find({seasonNumber: body.seasonNumber, series: body.series});
+            }
         }
-    } else { //season number was provided. Return all episodes with the provided season number
-        if(body.series == null) { //no series was provided. Return all episodes with provided season number
-            cursor = await episodes.find({seasonNumber: body.seasonNumber});
-        } else { //series was provided. Return all episodes with the provided seasonNumber and series tag
-            cursor = await episodes.find({seasonNumber: body.seasonNumber, series: body.series});
-        }
+    } else { //You can look for searchTerm key in the body of the request
+        cursor = await episodes.find({title: new RegExp(body.searchTerm, "i")});
     }
 
     cursor.toArray((err, docs) => {
@@ -77,4 +103,27 @@ async function processEpisodes(body, episodes, res, client) {
         client.close();
     });
     
+}
+
+async function processSeasons(episodes, res, client) {
+    console.log("Querying episodes collection to get a list of all the season numbers");
+
+    episodes.distinct("seasonNumber").then((seasons) => {
+        res.status(200).json({seasons: seasons});
+        client.close();
+    }, (err) => {
+        console.log(`error occured when attempting to retrieve season numbers: ${err}`);
+    });
+
+}
+
+async function processSeries(episodes, res, client) {
+    console.log("Querying episodes collection to get a list of all the series");
+
+    episodes.distinct("series").then((series) => {
+        res.status(200).json({series: series});
+        client.close();
+    }, (err) => {
+        console.log(`error occured when attempting to retrieve series: ${err}`);
+    });
 }
