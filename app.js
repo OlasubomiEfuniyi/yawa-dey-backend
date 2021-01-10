48890bc549ab1fa9d0c6e0f99ccda0bf82563262
Olasubomi Efuniyi <subsefun@gmail.com>
2021-01-10 16:51:46 -0500

    Making changes to fix cor issues

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const port = 8080;
 
 
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'https://yawa-dey-website.herokuapp.com');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET', 'POST', 'OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
