b3a5c91a0d11d042e1daae198be637713c488301
Olasubomi Efuniyi <subsefun@gmail.com>
2021-01-10 17:01:10 -0500

    Making changes to fix cor issues

--- app.js
+++ app.js
@@ -14,22 +14,13 @@ const helmet = require("helmet");
 const app = express();
 const port = 8080;
 
-
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET', 'POST', 'OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-    if(req.method === 'OPTIONS') {
-        res.sendStatus(200);
-    } else {
-        next();
-    }
-}
+const corsOptions = {
+    origin: "https://yawa-dey-website.herokuapp.com",
+    optionsSuccessStatus: 200
+};
 
 app.use(express.urlencoded({extended: true}));
-app.use(allowCrossDomain);
-app.use(cors()); //Needed to allow cross origin requests
+app.use(cors(corsOptions)); //Needed to allow cross origin requests
 app.use(express.json());
 app.use(compression()); //Compress all routes
 app.use(helmet()); //Protect app from well-know web vulnerabilities
